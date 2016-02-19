5297f176785bbfc2f8ad9a76cce74ca584c2f13a
Daniel Howe <daniel@rednoise.org>
2016-02-19 20:50:46 +0800

    added make p5 script

--- make-poetic-caption.sh
+++ make-poetic-caption.sh
@@ -2,5 +2,8 @@
 
 cd bin
 jar cvf ../p5/PoeticCaption/code/PoeticCaption.jar trp data
-cp -r ../rita1127.jar ../p5/PoeticCaption/code/
+cp -r ../rita.jar ../p5/PoeticCaption/code/
+echo
 ls -R ../p5/PoeticCaption/
+echo
+echo done
